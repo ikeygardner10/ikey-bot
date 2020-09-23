bbcdf9987a9edcdc5b7b4bbb7fad5dade18035ab
ikeygardner10 <ikeygardner10@gmail.com>
2020-09-23 23:38:46 +1200

    Create website.js

---
+++ commands/info/website.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	config: {
+		name: 'website',
+		aliases: ['web'],
+		usage: '',
+		cooldown: 10,
+		category: 'info',
+		permissions: '',
+		args: false,
+		description: 'Link for the website',
+	},
+	execute: async (client, message, args) => {
+
+		const wEmbed = new MessageEmbed()
+			.setAuthor(`${client.user.username}`, client.user.avatarURL())
+			.setDescription('**Website:** [Link](https://ikeybot.github.io)')
+			.setTimestamp()
+			.setColor('0xFFFFFA');
+
+		return message.channel.send(wEmbed);
+	} };
